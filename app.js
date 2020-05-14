ebf8b50ccda304e45ff970ef177d722201e6a752
SHIHCHANG <axray@hotmail.com>
2020-05-14 16:40:52 +0800

    finshed route with static html

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 
 const app = express();
 
-app.use('/',(req, res , next) => {
-    console.log('This always run');
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res , next) => {
-    console.log('In the middleware');
-    res.send('<h1>The Add Product</h1>')
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',(req, res , next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello World</h1>')
-})
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
